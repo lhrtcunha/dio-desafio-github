becf0cf923eafe1d5852382b998abbbfea5ac460
Luís Henrique <lhrtcunha@yahoo.com.br>
2021-12-16 11:44:39 -0300

    Alterações diversas

--- dotnet/Construtores/ExemploConstrutores/Models/Calculadora.cs
+++ dotnet/Construtores/ExemploConstrutores/Models/Calculadora.cs
@@ -7,9 +7,19 @@ namespace ExemploConstrutores.Models
 {
     public class Calculadora
     {
+        public delegate void DelegateCalculadora();
+        public static event DelegateCalculadora? EventoCalculadora;
         public static void Somar(int x, int y)
         {
-            System.Console.WriteLine($"Adição: {x + y}");
+            if (EventoCalculadora != null)
+            {
+                System.Console.WriteLine($"Adição: {x + y}");
+                EventoCalculadora();
+            }
+            else
+            {
+                System.Console.WriteLine("Nenhum inscrito!");
+            }
         }
         public static void Subtrair(int x, int y)
         {
--- dotnet/Construtores/ExemploConstrutores/Program.cs
+++ dotnet/Construtores/ExemploConstrutores/Program.cs
@@ -1,25 +1,39 @@
-﻿using ExemploConstrutores.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExemploConstrutores.Models;
 
-// Pessoa p1 = new Pessoa("Luís Henrique","R. T. da Cunha");
-// p1.Apresentar();
+namespace ExemploConstrutores // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Pessoa p1 = new Pessoa("Luís Henrique","R. T. da Cunha");
+            // p1.Apresentar();
 
-// Log log = Log.GetInstance();
+            // Log log = Log.GetInstance();
 
-// log.PropriedadeLog = "Teste Instância";
+            // log.PropriedadeLog = "Teste Instância";
 
-// Log log2 = Log.GetInstance();
-// System.Console.WriteLine(log2.PropriedadeLog);
+            // Log log2 = Log.GetInstance();
+            // System.Console.WriteLine(log2.PropriedadeLog);
 
-// Aluno a1 = new Aluno("Luís Henrique","R. T. da Cunha","Teste");
-// a1.Apresentar();
+            // Aluno a1 = new Aluno("Luís Henrique","R. T. da Cunha","Teste");
+            // a1.Apresentar();
 
-// Data d1 = new Data();
-// d1.SetMes(20);
-// d1.ApresentarMes();
-// d1.Mes = 2;
-// d1.ApresentarMes();
+            // Data d1 = new Data();
+            // d1.SetMes(20);
+            // d1.ApresentarMes();
+            // d1.Mes = 2;
+            // d1.ApresentarMes();
 
-// const double pi = 3.14;
-// System.Console.WriteLine(pi);
+             //const double pi = 3.14;
+             //System.Console.WriteLine(pi);            
 
+            Matematica m = new Matematica(10,20);
+            m.Somar();
+        }
+    }
+}
 
